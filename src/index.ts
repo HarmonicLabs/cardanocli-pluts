4e2f064393593fd8813544fa5375eef6884353e1
michele-nuzzi <michele.nuzzi.2014@gamil.com>
2023-02-09 11:18:01 +0100

    type: commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export * from "./CardanoCliPluts";
+import { CardanoCliPluts } from "./CardanoCliPluts/CardanoCliPluts";
+
+export * from "./CardanoCliPluts/cliCmds";
+export { CardanoCliPluts } from "./CardanoCliPluts";
+
 export * from "./errors";
 export * from "./types/CardanoEra";
+
+export default CardanoCliPluts;
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const hello = require("../dist/CardanoCliPluts/CardanoCliPluts.js");
+
+console.log( hello.CardanoCliPluts );
